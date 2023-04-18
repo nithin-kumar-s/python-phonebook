b5eda50339a97501c28d81406333b5cabf42b2d5
nithin-kumar-s <snithinkumar170@gmail.com>
2023-04-18 19:32:59 +0530

    this is demo commit for our project

---
+++ calculator.py
@@ -0,0 +1,12 @@
+from tkinter import*
+from tkinter import ttk
+
+co0 = "#ffffff"
+co1 = "#000000"
+co9 = "#4456F0"
+
+window= Tk()
+window.title("")
+window.geometry('485x450')
+window.configure(background=co0)
+window.resizable(width=FALSE, height=FALSE)
